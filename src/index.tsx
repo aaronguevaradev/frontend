1a80f3a1d50cb915b536b3068648818c7c09e209
Aaron Guevara <aaronguevaradev@gmail.com>
2020-12-24 08:16:36 -0800

    added material-ui library for quick branding and modern UI support

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'fontsource-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </ThemeProvider>,
+ 
   document.getElementById('root')
 );
 
